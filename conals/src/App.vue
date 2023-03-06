4c9f784c8ef9bcb2ea1672d477556f8d5b5b5582
Imeguras <imeguras@teknik.io>
2023-03-06 23:13:10 +0000

    Pilot

---
+++ conals/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+	<Canvas/>
+  </div>
+</template>
+
+<script>
+import Canvas from './components/Canvas.vue'
+export default {
+  name: 'App',
+components: {
+	//use Canvas.vue
+	Canvas
+
+},
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  position: absolute;
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  background: #2c3e50;
+  top: 0px;
+  left: 0px;
+
+}
+</style>
---
+++ conals/src/components/Canvas.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+div
+  v-stage(width="500" height="500" :config="stageOptions")  
+    v-layer(v-if="!loaded")
+      v-rect(x="0" y="0" width="100" height="100" fill="red" )
+      v-rect(x="100" y="100" width="100" height="100" fill="blue" )
+      v-rect(x="200" y="200" width="100" height="100" fill="green")
+    v-layer(v-if="loaded")
+      v-image(
+		:image="image" 	  	
+	  )
+      
+
+</template>
+<script>
+//
+import { Stage, Layer, Image } from 'vue-konva'
+export default {
+  name: 'Canvas',
+  components: {
+	Stage,
+	Layer,
+	Image
+  },
+  data () {
+	return {
+	  loaded: false,
+	  stageOptions: {
+		width: window.innerWidth,
+		height: window.innerHeight
+	  
+	  }, 
+	  image: null
+
+	}
+  }, 
+  created() {
+
+	const image = new window.Image();
+	
+    image.src = "./img/logo.jpg";
+    image.onload = () => {
+		this.loaded = true;
+      // set image only when it is loaded
+      this.image = image;
+    
+	};
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
